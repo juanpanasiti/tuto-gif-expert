1b6342eea5c5bbfb1d6ce5eff08fa7bdc9b27215
Juan Panasiti <juanpanasiti@gmail.com>
2020-11-22 22:37:52 -0300

    implementadas animaciones de animate.style

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const GifGridItem = ({ id, title, url }) => {
   return (
-    <div className='card'>
+    <div className='card animate__animated animate__fadeIn'>
       <img src={url} width='150px' alt={title} />
       <br />
       <p>
